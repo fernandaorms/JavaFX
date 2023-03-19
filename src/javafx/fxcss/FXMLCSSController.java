eb910ef5b736a6bf632b23b85946a5ebb5a795cf
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2023-03-19 10:08:54 -0300

    JavaFX CSS

---
+++ src/javafx/fxcss/FXMLCSSController.java
@@ -0,0 +1,15 @@
+package javafx.fxcss;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class FXMLCSSController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+      
+    }
+    
+}
